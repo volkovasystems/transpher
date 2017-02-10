ffe0430af8037b8e399e6c13a5e3e0a34f326b92
johnlenon <johnlenonmaghanoy@gmail.com>
2017-02-10 19:45:42 +0800

    Updated test.

--- transpher-test.js
+++ transpher-test.js
@@ -1,3 +1,7 @@
+"use strict";
+
 const transpher = require( "./transpher.js" );
 
-console.log( transpher( { "name": "simple" }, { "type": "ok" } ) )
+console.log( transpher( { "name": "simple" }, { "type": "ok" } ) );
+console.log( transpher( { } ) );
+console.log( transpher( ) );
